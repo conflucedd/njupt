5a2624be3b4795c728ec17da9354e6af0427b723
confluced <confluced@gmail.com>
2024-10-28 14:15:37 +0800

    sdv

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let message: String = fs::read_to_string("/tmp/a")?;
+    println!("{}", message);
+    Ok(())
 }
