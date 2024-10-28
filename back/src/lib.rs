cbeaeed30a8423e1a9c2613badbba88e98a0cc77
confluced <confluced@gmail.com>
2024-10-28 16:34:36 +0800

    complete for base back framework

---
+++ back/src/lib.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+enum Area {
+    Unclicked,
+    Marked,
+    Known
+}
+
+
+fn send(a: String) -> () {
+
+}
+
+fn recv() -> String {
+    // message = fs::read_to_string("/tmp/a").unwrap();
+}
+
+fn extract_size(a: String) -> i32 {
+
+}
+
+fn extract_position(a: String) -> (i32, i32) {
+
+}
+
+
+fn boom (i: i32, j: i32) -> bool {
+
+}
+
+fn auto_expand(a: Vec<Vec<Area>>) -> () {
+
+}
+
+fn check_win(a: Vec<Vec<Area>>) -> bool {
+    
+}
--- back/src/main.rs
+++ back/src/main.rs
@@ -1,8 +1,54 @@
-use std::fs;
-use std::error::Error;
-use back::
-fn main() -> Result<(), Box<dyn Error>> {
-    let message: String = fs::read_to_string("/tmp/a")?;
-    println!("{}", message);
-    Ok(())
+
+fn main() {
+    let message: String;
+    let mut size: i32;
+    let mut checkerboard: Vec<Vec<Area>> = Vec::new();
+
+    loop  {
+        message = recv();
+        println!("{}", message); // for debug
+        match &message {
+            "~start" => {
+                size = extract_size(message);
+                for i in size {
+                    let mut vec = Vec::new();
+                    for j in size{
+                        let a = Area::Unclicked;
+                        vec.push(a);
+                    }
+                    checkerboard.push(vec);
+                }
+                send("~OK$");
+                continue;
+            }
+            "~click" => {
+                let position = extract_position(message);
+                if boom(position) {
+                    send
+                }
+                else {
+                    auto_expand(checkerboard);
+                }
+                continue;
+            }
+            "~mark" => {
+                let position = extract_position(message);
+                checkerboard[i][j] = Area::Marked;
+                continue;
+            }
+            "~abort" => {
+                send("~OK$");
+                continue;
+            }
+        }
+
+        if check_win(checkerboard)
+        {
+            send("~win$");
+        }
+        else
+        {
+            send("~lose");
+        }
+    }
 }
